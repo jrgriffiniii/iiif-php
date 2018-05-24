a878557f5bb2b45a8a0338b600e16701e843a1c1
James R. Griffin III <jrgriffiniii@gmail.com>
2018-05-24 12:27:33 -0400

    Documenting the Region Class

--- src/Model/Region.php
+++ src/Model/Region.php
@@ -2,16 +2,27 @@
 
 namespace IIIF\Model;
 
+/**
+ * Class modeling <a href ="http://iiif.io/api/image/2.1/#region">rectangular regions within image resources</a>
+ */
 class Region
 {
-    use WithMetaData;
+    use WithMetaData; /**< Mixes in the WithMetaData Trait */
 
-    private $unit;
-    private $x;
-    private $y;
-    private $width;
-    private $height;
+    private $unit; /**< @var string units for the specified region */
+    private $x; /**< @var int horizontal offset (units from 0 on the horizontal axis) for the specified region */
+    private $y; /**< @var int vertical offset (units from 0 on the vertical axis) for the specified region */
+    private $width; /**< @var int width of the region */
+    private $height; /**< @var int height of the region */
 
+    /**
+     * Constructor
+     * @param string $unit units for the specified region
+     * @param int $x horizontal offset for the specified region
+     * @param int $y vertical offset for the specified region
+     * @param int $width width of the region
+     * @param int $height height of the region
+     */
     public function __construct(string $unit, int $x, int $y, int $width, int $height)
     {
         $this->unit = $unit;
@@ -21,28 +32,49 @@ class Region
         $this->height = $height;
     }
 
+    /**
+     * Accessor method for the horizontal offset
+     * @return int
+     */
     public function getX()
     {
         return $this->x;
     }
 
+    /**
+     * Accessor method for the vertical offset
+     * @return int
+     */
     public function getY()
     {
         return $this->y;
     }
 
+    /**
+     * Accessor method for the height
+     * @return int
+     */
     public function getHeight()
     {
         return $this->height;
     }
 
+    /**
+     * Accessor method for the width
+     * @return int
+     */
     public function getWidth()
     {
         return $this->width;
     }
 
-    const W3C_REGEX = '/[#&\?]xywh\=(pixel\:|percent\:)?(\d+),(\d+),(\d+),(\d+)/';
+    const W3C_REGEX = '/[#&\?]xywh\=(pixel\:|percent\:)?(\d+),(\d+),(\d+),(\d+)/'; /**< @var regular expressing for parsing the bounding box from a URI */
 
+    /**
+     * Constructs a Region object from a URI containing the bounding box
+     * @param string $url URI containing the bounding box
+     * @return Region
+     */
     public static function fromUrlTarget($url)
     {
         $matches = [];
@@ -57,6 +89,15 @@ class Region
         );
     }
 
+    /**
+     * Constructs a Region object using the horizontal and vertical offsets, height, and width
+     * (Defaults to using pixels as units)
+     * @param int $x horizontal offset for the specified region
+     * @param int $y vertical offset for the specified region
+     * @param int $height height of the region
+     * @param int $width width of the region
+     * @return Region
+     */
     public static function create($x, $y, $h, $w)
     {
         return new static(
